3f4c14a9df472fd41bf7e15abc9af490d89b93cd
kvwadhwa <82910611+kvwadhwa@users.noreply.github.com>
2023-03-05 17:32:28 -0800

    Adding list and delete APIs

--- memlinkto_project/memlinkto_app/rpc.py
+++ memlinkto_project/memlinkto_app/rpc.py
@@ -24,3 +24,17 @@ def create_link(short_url: str, long_url: str, **kwargs):
 def fetch_link(short_url: str):
     url_mapping: UrlMapping = UrlMapping.objects.get(short_url=short_url)
     return url_mapping.long_url
+
+@rpc_method
+def delete_link(short_url: str, long_url: str, **kwargs):
+    request = kwargs.get(REQUEST_KEY)
+    email_address = EmailAddress.objects.get(user_id=request.user)
+    url_mapping = UrlMapping(email_address=email_address, short_url=short_url, long_url=long_url)
+    url_mapping.save()
+    return short_url
+@rpc_method
+def list_links(**kwargs):
+    request = kwargs.get(REQUEST_KEY)
+    email_address = EmailAddress.objects.get(user_id=request.user)
+    url_mappings = UrlMapping.objects.filter(email_address=email_address)
+    return list(map(lambda x: {"short_url": x.short_url, "long_url": x.long_url}, list(url_mappings)))
