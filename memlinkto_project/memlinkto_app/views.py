86c77507a02bcaa338cb0e4f935373f143ea5290
Kunal Wadhwa <kwadhwa@lakeheadu.ca>
2023-04-04 21:15:41 -0700

    Revert "Updated tensorflow version"
    This reverts commit 11008e1bed55797413c69bec3bd615c6d623faf9.

--- memlinkto_project/memlinkto_app/views.py
+++ memlinkto_project/memlinkto_app/views.py
@@ -4,26 +4,6 @@ from django.template import loader
 from django.shortcuts import redirect
 from django.views.defaults import page_not_found
 from memlinkto_app.models import UrlMapping
-import requests
-from bs4 import BeautifulSoup
-import tensorflow.compat.v1 as tf
-import tensorflow_hub as hub
-from slugify import slugify
-import re
-
-tf.disable_v2_behavior()
-text_generator = hub.Module('/Users/jeetub/Downloads/roberta24')
-
-# Create placeholders for the input data
-input_placeholder = tf.placeholder(dtype=tf.string, shape=[None])
-
-# Apply the module to the input data
-output_summaries = text_generator(input_placeholder)
-
-# Create a TensorFlow session
-session = tf.Session()
-session.run(tf.global_variables_initializer())
-session.run(tf.tables_initializer())
 
 def index(request):
     template_file = 'index.html'
@@ -49,19 +29,8 @@ def links(request):
         result.append(entry)
     return HttpResponse(template.render({"url_mappings": result}, request))
 
-
 def redirect_or_404(request, exception):
     url_mappings = UrlMapping.objects.filter(short_url=request.build_absolute_uri())
     if len(url_mappings) > 0:
         return redirect(url_mappings[0].long_url)
     return page_not_found()
-
-
-def generate_slug(url):
-    html = requests.get(url).text
-    text_content = BeautifulSoup(html, 'html.parser').get_text()
-    summaries = session.run(output_summaries, feed_dict={input_placeholder: [text_content]})
-    summary = summaries[0].decode('utf-8')
-    summary_no_articles = ' '.join(
-        [word for word in re.findall(r'\b\w+\b', summary) if word.lower() not in ['a', 'an', 'the']])
-    return slugify(summary_no_articles)
