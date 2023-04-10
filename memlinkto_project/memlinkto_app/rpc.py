53483ae9f8b9858f763e5563bec547ece846e67c
tahmidul612 <tahmidul612@gmail.com>
2023-04-10 03:32:21 -0400

    Add Google safe browsing api check

--- memlinkto_project/memlinkto_app/rpc.py
+++ memlinkto_project/memlinkto_app/rpc.py
@@ -2,6 +2,7 @@ from allauth.account.models import EmailAddress
 from modernrpc.core import rpc_method, REQUEST_KEY
 import uuid
 import requests
+import json
 
 from memlinkto_app.models import UrlMapping
 
@@ -27,6 +28,49 @@ def create_link(long_url: str, **kwargs):
                              short_url=short_url, long_url=long_url)
     url_mapping.save()
     return short_url
+
+
+@rpc_method
+def link_is_safe(input_url: str):
+    api_key = 'AIzaSyDzJERZizEEmzTCkLoScuygU_gtCnKT2dA'
+    url = f'https://safebrowsing.googleapis.com/v4/threatMatches:find?key={api_key}'
+
+    # Define the request body as a dictionary
+    request_body = {
+        'client': {
+            'clientId':      'memlink-to',
+            'clientVersion': '0.0.1'
+        },
+        'threatInfo': {
+            'threatTypes':      ['MALWARE', 'SOCIAL_ENGINEERING', 'POTENTIALLY_HARMFUL_APPLICATION'],
+            'platformTypes':    ['WINDOWS'],
+            'threatEntryTypes': ['URL'],
+            'threatEntries':    [{'url': input_url}]
+        }
+    }
+
+    # Convert the request body to a JSON string
+    json_data = json.dumps(request_body)
+
+    # Make the request with the appropriate headers
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, headers=headers, data=json_data)
+
+    # Check if the request was successful and process the response
+    if response.status_code == 200:
+        response_data = response.json()
+        # Process the response data as needed
+        if not response_data:
+            print("Site is safe")
+            return "safe"
+        else:
+            threat_matches = response_data.get('matches', [])
+            for match in threat_matches:
+                print(f'Site not safe, Threat type: {match["threatType"]}')
+            return "not safe"
+    else:
+        print(f'Request failed with status code {response.status_code}')
+        return "failed"
 
 
 @ rpc_method
